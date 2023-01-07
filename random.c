bd8c6b1831967de0d438c2b789f2ddf63dbb497e
Ait Hamid Adam <adam.aithamid@edu.esiee.fr~>
2023-01-07 15:59:43 +0100

    IL MANQUE LE DERNIER RANDOM

--- random.c
+++ random.c
@@ -2,23 +2,53 @@
 #include <stdlib.h>
 
 double random_double(double a, double b){
-
+    if(b>a)
+    {
+        double c =a;
+        a = b;
+        b = c;
+    }
+    return ((double)rand()/(double)RAND_MAX)*(a-b)+b;
 }
 
 float random_float(float a, float b){
-
+     if(b>a)
+    {
+        float c =a;
+        a = b;
+        b = c;
+    }
+    return ((float) rand() / (float) RAND_MAX)*(a-b)+b;
 }
 
 size_t random_size_t(size_t a, size_t b){
-
+    if(b>a)
+    {
+        size_t c =a;
+        a = b;
+        b = c;
+    }
+    return (size_t) (rand() % (a-b))+b;
 }
 
 int random_int(int a, int b){
-
+    if(b>a)
+    {
+        int c =a;
+        a = b;
+        b = c;
+    }
+    return (int) (rand() % (a-b))+b;
 }
 
 char random_char(char a, char b){
-
+    if(b>a)
+    {
+        char c =a;
+        a = b;
+        b = c;
+    }
+    return (char) (rand() % (a-b))+b;
 }
 
 void random_init_string(char * c, size_t n){
--- test_random.c
+++ test_random.c
@@ -6,6 +6,15 @@
 
 int main(int argc, char*argv[]){
     srand(time(NULL));
-
+    time_t t;
+    srand((unsigned) time(&t));
+    printf("%d\n",random_int(4,10));
+    printf("%c\n",random_char('B','D'));
+    printf("%d\n",random_size_t(4,9));
+    printf("%f\n",random_double(5.00,10.00));
+    printf("%f\n",random_float(5.00,10.00));
+    double X=((double)rand()/(double)RAND_MAX);
+    printf("%lf",X);
     return 0;
+    
 }
